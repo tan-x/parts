36bd1e436b023809b9d670eedf698d0a31c39d60
Tanner Griffin <tannermgriffin@gmail.com>
2020-10-09 15:16:28 -0500

    Created state for filter modal and filter button to open modal

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Table from '../components/PartTable';
 import 'semantic-ui-css/semantic.min.css';
 import styles from '../styles/Home.module.css';
-import { Dropdown, Checkbox, Input } from 'semantic-ui-react';
+import { Dropdown, Checkbox, Input, Button, Icon } from 'semantic-ui-react';
 const options = [10, 20, 50];
 const rowOptions = options.map((i) => ({
 	key: i,
@@ -14,18 +14,23 @@ const rowOptions = options.map((i) => ({
 export default function Home() {
 	const [rows, setRows] = useState(10);
 	const [dark, setDark] = useState(false);
-	const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
+  const [modal, setModal] = useState({filter: false})
 	const rowChange = (e, { value }) => {
 		setRows(value);
+	};
+	const searchChange = ({ target }) => {
+		let searchValue;
+		if (typeof target.value === 'number') {
+			searchValue = parseInt(target.value);
+		} else {
+			searchValue = target.value.toLowerCase();
+		}
+		setSearch(searchValue);
   };
-  const searchChange = ({target}) => {
-    let searchValue;
-    if (typeof target.value === 'number') {
-      searchValue = parseInt(target.value);
-    } else {
-      searchValue = target.value.toLowerCase();
-    }
-    setSearch(searchValue);
+  
+  const filterModal = (e) => {
+    setModal({...modal, filter: !modal.filter})
   }
 	return (
 		<div className={!dark ? styles.container : styles.dark}>
@@ -38,16 +43,21 @@ export default function Home() {
 				<Checkbox toggle onChange={() => setDark(!dark)} />
 			</header>
 			<main className={styles.main}>
-				<h1 className={styles.title} style={{ color: dark && 'white' }}>
+				<h1 className={styles.title} style={{ color: dark && 'white', marginBottom: 10 }}>
 					Parts
 				</h1>
-				<div>
+				<div style={{ display: 'flex', justifyContent: 'space-between', width: '95vw', maxWidth: '600px', height: 40 }}>
+					<Button size="mini" compact onClick={filterModal}>
+						<Icon name='filter' />
+						Filter
+					</Button>
 					<Input placeholder='Search' value={search} onChange={searchChange} />
-					<span style={{ margin: 10, color: dark && 'white' }}>Rows</span>
-					<Dropdown compact selection defaultValue={rows} options={rowOptions} onChange={rowChange} />
+					<div style={{display: 'flex', alignItems: 'center'}}>
+						<span style={{ margin: 5, color: dark && 'white' }}>Rows</span>
+						<Dropdown compact selection defaultValue={rows} options={rowOptions} onChange={rowChange} />
+					</div>
 				</div>
-
-				<Table rows={rows} dark={dark} search={search}/>
+				<Table rows={rows} dark={dark} search={search} />
 			</main>
 		</div>
 	);
