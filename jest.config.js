121bff8cda49e0907618a7572c1c3b8a264e1d06
Tanner Griffin <tannermgriffin@gmail.com>
2020-10-09 13:28:54 -0500

    Altered jest config to test circle ci issue

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 	setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-	transform: {
-		'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-	},
+	// transform: {
+	// 	'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+	// },
 	moduleNameMapper: {
 		"\\.(css|less)$": "<rootDir>/node_modules/jest-css-modules"
 	}
