e56e45b702a025bce86d68c7fa719a388d2324d9
Tanner Griffin <tannermgriffin@gmail.com>
2020-10-09 12:32:00 -0500

    Created Pagination components to reduce code length. Removed footer from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Head from 'next/head';
-import Table from '../components/Table';
+import Table from '../components/PartTable';
 import 'semantic-ui-css/semantic.min.css';
 import styles from '../styles/Home.module.css';
 import { Dropdown, Checkbox } from 'semantic-ui-react';
@@ -14,7 +14,6 @@ const rowOptions = options.map((i) => ({
 export default function Home() {
 	const [rows, setRows] = useState(10);
 	const [dark, setDark] = useState(false);
-
 	const rowChange = (e, { value }) => {
 		setRows(value);
 	};
@@ -24,29 +23,16 @@ export default function Home() {
 				<title>Parts</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-
 			<header className={styles.header}>
 				<span style={{margin: 10, color: dark && 'white'}}>Dark Mode</span>
 				<Checkbox toggle onChange={() => setDark(!dark)} />
 			</header>
-
 			<main className={styles.main}>
 				<h1 className={styles.title} style={{color: dark && 'white'}}>Parts</h1>
 				<span style={{margin: 10, color: dark && 'white'}}>Rows</span>
 				<Dropdown compact selection defaultValue={rows} options={rowOptions} onChange={rowChange} />
 				<Table rows={rows} dark={dark} />
 			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-          rel='noopener noreferrer'
-          style={{color: dark && 'white'}}
-				>
-					Powered by <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo}/>
-				</a>
-			</footer>
 		</div>
 	);
 }
